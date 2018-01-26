4b9bb3c1df5d92e886418fcd7472205c6a8916ee
pbarzilai <pba429@gmail.com>
2018-01-26 20:10:28 +0200

    using curl instead of dig for retrieving IP

--- check_updated_customer_ip.sh
+++ check_updated_customer_ip.sh
@@ -5,13 +5,12 @@ do
 	then
 		continue
 	else
-		HOST_FINAL="$(curl -Ls -o /dev/null -w %{url_effective} $HOST1 | sed -e 's/http:\/\///g' -e 's/https:\/\///g' -e 's/\/.*$//')" #Get final hostname after redirections
-		HOST_IP="$(dig $HOST_FINAL +noall +answer | grep $'IN\tA' | grep -v 'IP ADDRESS SUBNET eg: 192.168.0')" #Check if new IP address is different from customers IP subnet (grep -v)
+		HOST_IP="$(curl -Ls -o /dev/null -w "%{url_effective}  %{remote_ip}" $HOST1 | grep -v 'IP ADDRESS SUBNET eg: 192.168.0')" #Check if new IP address is different from customers IP subnet (grep -v)
 		if [ -z "$HOST_IP" ]
 		then
 			continue
 		else
-			echo $HOST_FINAL
+			echo $HOST1
 			echo $HOST_IP
 			printf "\n"
 		fi
